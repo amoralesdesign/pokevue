cdf20b7de0a7253a40ba3c31619a2b2291cb61e4
amoralesrp <amorales@foticos.com>
2022-06-28 13:01:56 +0200

    transition

--- src/components/PokeModal.vue
+++ src/components/PokeModal.vue
@@ -1,6 +1,6 @@
 <!-- Modal component -->
 <template>
-  <transition name="modal-fade">
+  <Transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100">
     <div class="modal-overlay" @click="$emit('close-modal')">
       <div class="modal relative" @click.stop>
         <div class="z-40 absolute cursor-pointer right-4 top-4 uppercase" @click="$emit('close-modal')">
@@ -9,5 +9,5 @@
         <slot name="content"></slot>
       </div>
     </div>
-  </transition>
+  </Transition>
 </template>
